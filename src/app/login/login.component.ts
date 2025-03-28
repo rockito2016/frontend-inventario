7741842659ab299ea9f328c51ed6121fd9d203e6
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2025-03-28 15:13:36 -0500

    sincambios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent {
       contrasena: this.password
     };
 
-    this.http.post<any>('https://backend-26uovc81i-javier-pascuaza-s-proyectos.vercel.app/api/login', loginData).subscribe(
+    this.http.post<any>('http://localhost:3000/api/login', loginData).subscribe(
       (response) => {
         this.loading = false;
         if (response.success) {
